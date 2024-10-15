1887382ab68f78d6fe555d9bd6a8d2a5691f6e1e
Alessandra Rodrigues <alessandrarjrodrigues19@gmail.com>
2024-10-15 11:52:03 -0300

    Create app01.py

---
+++ app01.py
@@ -0,0 +1,50 @@
+# Cria uma variável
+
+'''
+Esse comentário pode
+ter muitas
+e muitas
+linhas
+'''
+
+'''
+Esse comentário pode
+ter muitas
+e muitas
+linhas
+
+'''
+
+# Cria uma variável usando "snake_case"
+cor_da_casa ='azul'
+
+# cria uma variável usando "camelcase"
+cordacasa = "amarela"
+
+# Exibir as coisas
+print('\n\n\n')
+print(cor_da_casa)
+print(cordacasa)
+
+
+# Exibir as coisa
+print(cor_da_casa)
+ 
+
+# primeiro nome
+first_name = 'Joca'
+
+
+# ùltimo nome
+last_name = "Silva" 
+
+# Imprime meu nome
+print(first_name)
+print(last_name)
+
+# Imprima os dois nomes
+print(first_name, last_name)
+
+
+
+
